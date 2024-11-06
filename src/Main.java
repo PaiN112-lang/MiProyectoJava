448559ededbb9ba9676751e2adc56d0eac4c42ac
PaiN112-lang <mdiamen1112@g.educaand.es>
2024-11-06 09:30:02 +0100

    Merge master - nueva

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hola, mundo! Esta es una nueva funcionalidad");
+        System.out.println("Hola, mundo! Esta es una nueva funcionalidad ");
 
 
 
